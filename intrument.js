b329ec2be384e00c69f64658b334b0078de5ad9e
SS-y1 <78398069+SS-y1@users.noreply.github.com>
2023-03-18 02:09:22 +0800

    Update intrument.js

--- intrument.js
+++ intrument.js
@@ -39,7 +39,7 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);  //创建画布
+    createCanvas(1800, 900);  //创建画布
 
     colorMode(HSB);
     strokeCap(ROUND); //PROJECT for rectangles, ROUND for circles and rounded rectangles
